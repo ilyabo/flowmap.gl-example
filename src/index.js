0e5b8bf8ae53546ba8d8a5b13b9b423af3f8dccd
jh-gis <84340072+jh-gis@users.noreply.github.com>
2021-10-19 10:55:04 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 document.write(
-  `<a href="https://github.com/ilyabo/flowmap.gl-example">
+  `<a href="https://github.com/jh-gis/flowmap.gl-example">
     <img 
       style="position: absolute; top: 0; right: 0; border: 0;" 
       src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" 
